6dc5d0febf239213f6f05f1717164f41f63673d5
Michelle Hu <binwuhu@outlook.com>
2024-09-27 14:08:32 -0400

    Add update password in backend using @sendgrid/mail

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authUser, registerUser, logoutUser, getUserProfile } from '../controllers/userController.js';
+import { authUser, registerUser, logoutUser, getUserProfile, forgotPassword, resetPassword } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,7 @@ router.post('/login', authUser);
 router.post('/register', registerUser);
 router.post('/logout', logoutUser);
 router.route('/profile').get(protect, getUserProfile);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 export default router;
