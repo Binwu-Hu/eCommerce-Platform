e9af283b91261936dfdfdea36ea854f28b53d828
Zhiyuan Jiang <zhiyuanj@andrew.cmu.edu>
2024-09-28 20:54:32 -0400

    implement product displaying page

---
+++ frontend/src/pages/product/Products.tsx
@@ -0,0 +1,21 @@
+import CreateProductForm from '../../components/product/CreateProductForm'; // Only show if admin
+import ProductList from '../../components/product/ProductList';
+import React from 'react';
+
+const ProductPage: React.FC = () => {
+  const isAdmin = true; // Assuming you have a way to determine admin status
+
+  return (
+    <div className='container mx-auto py-8 px-4'>
+      {isAdmin && (
+        <div className='mb-6'>
+          <CreateProductForm />
+        </div>
+      )}
+
+      <ProductList />
+    </div>
+  );
+};
+
+export default ProductPage;
