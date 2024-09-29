01b09486be756d8a8752d4fd4569b7052725c069
Michelle Hu <binwuhu@outlook.com>
2024-09-29 05:27:59 -0400

    Add Error page to prevent unauthorized user to create product

--- frontend/src/pages/product/createProduct.tsx
+++ frontend/src/pages/product/createProduct.tsx
@@ -1,14 +1,20 @@
 import CreateProductForm from '../../components/product/CreateProductForm';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
+import ErrorPage from '../error/ErrorPage'; 
 
 const ProductPage: React.FC = () => {
-  const isAdmin = true;
+  const isAdmin = useSelector((state: RootState) => state.user?.user?.isAdmin);
+
   return (
     <div className='container mx-auto py-8 px-4'>
-      {isAdmin && (
+      {isAdmin ? (
         <div className='mb-6'>
           <CreateProductForm />
         </div>
+      ) : (
+        <ErrorPage />
       )}
     </div>
   );
